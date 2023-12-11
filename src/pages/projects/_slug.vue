29e30ca78d7f6c21e8af6978c42e8dc7ddbad939
stephen <tasb00429@gmail.com>
2023-12-11 16:22:33 +0800

    feat: projects for render markdown content

---
+++ src/pages/projects/_slug.vue
@@ -0,0 +1,298 @@
+<script lang="ts">
+import Vue from "vue"
+
+/* Interfaces */
+import type { Post } from "~/src/types/Post"
+
+interface RelatedPost {
+  title: string
+  slug: string
+}
+
+export default Vue.extend({
+  data() {
+    return {
+      project: {} as Post,
+      related: [] as RelatedPost[],
+    }
+  },
+  async fetch() {
+    const project = (await this.$content(
+      "projects",
+      this.$route.params.slug
+    ).fetch()) as Post
+
+    if (!project) {
+      this.$router.push("/projects")
+      return
+    }
+
+    this.project = project
+
+    if (project.related?.length) {
+      const array = []
+
+      for (const key of project.related) {
+        const { title }: any = await this.$content("blog", key)
+          .only(["title"])
+          .fetch()
+
+        array.push({
+          title,
+          slug: key,
+        })
+      }
+
+      this.related = array
+    }
+  },
+  head() {
+    const project = this.project
+    const { getTags } = this as {
+      getTags: string[]
+    }
+
+    const title = project.title
+    const description =
+      project.description || ""
+
+    const tags = getTags?.join(", ") || title
+    const href = `https://eggsy.xyz${this.$route?.path}`
+    const image = `/og-images/${project.slug}.png`
+
+    return {
+      bodyAttrs: {
+        class: "overflow-x-hidden",
+      },
+      title,
+      link: [
+        {
+          rel: "canonical",
+          href,
+        },
+      ],
+      meta: this.$prepareMeta(
+        {
+          title,
+          description,
+          image,
+          keywords: `${tags}, stephen chen's project, project`,
+          url: href,
+        },
+        [
+          {
+            name: "twitter:card",
+            content: "summary_large_image",
+          },
+          {
+            name: "article:published-time",
+            content: project?.createdAt || null,
+          },
+          {
+            name: "twitter:label1",
+            content: "Tarih",
+          },
+          {
+            name: "twitter:data1",
+            content: project?.createdAt,
+          },
+          {
+            name: "twitter:label2",
+            content: "Okuma Süresi",
+          },
+          {
+            name: "twitter:data2",
+            content: `${this.getReadingTime} dakika`,
+          },
+        ]
+      ),
+    }
+  },
+  watch: {
+    $fetchState: {
+      async handler(state) {
+        if (state.pending === true || state.error !== null) return
+        await this.$nextTick()
+        this.$applyMediumZoom()
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    getTags(): string[] {
+      return this.project?.tags || []
+    },
+    getReadingTime() {
+      return this.$getReadingTime(JSON.stringify(this.project.body))
+    },
+    getReadableDate() {
+      return this.$getReadableDate(new Date(this.project?.createdAt || Date.now()))
+    },
+    getRelatedPosts(): RelatedPost[] {
+      return this.related || []
+    },
+  },
+})
+</script>
+
+<template>
+  <Transition name="fade">
+    <LoadersContent
+      v-if="$fetchState.pending === true || $fetchState.error !== null"
+      :error="$fetchState.pending === false && $fetchState.error !== null"
+    />
+
+    <div v-else class="pt-4 mt-10">
+      <article>
+        <header class="space-y-8 leading-relaxed text-center mb-18">
+          <img
+            v-if="project.header"
+            :src="project.header"
+            class="object-cover object-top w-full transition-all rounded-lg h-30 ring-1 dark:ring-white/10 ring-black/10 hover:h-90 hover:object-center duration-1000"
+            alt="Post header"
+          />
+
+          <div class="space-y-4">
+            <div
+              class="flex flex-wrap items-center justify-center gap-x-6 gap-y-2 dark:text-white/30 text-black/50 sm:text-sm"
+            >
+              <div class="flex items-center space-x-2">
+                <IconCalendar class="w-4 h-4" />
+                <span>{{ getReadableDate }}</span>
+              </div>
+
+              <div class="flex items-center space-x-2">
+                <IconTag class="w-4 h-4" />
+                <span>{{ getTags.join(" － ") }}</span>
+              </div>
+            </div>
+
+            <div class="space-y-2">
+              <h1
+                class="block mx-auto text-2xl font-bold text-black md:w-11/12 sm:text-4xl dark:text-white"
+              >
+                {{ project.title }}
+              </h1>
+
+              <p class="mx-auto text-black/50 md:w-9/12 dark:text-white/50">
+                {{ project.description }}
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <div class="mt-4">
+          <template v-if="!project.indicatorsHidden">
+            <div
+              class="sticky z-10 hidden float-left -ml-20 text-right top-4 md:block"
+            >
+              <BlogShare
+                type="vertical"
+                :title="project.title"
+                :path="$route.path"
+              />
+            </div>
+
+            <div
+              class="sticky z-10 hidden float-right text-left -mr-14 top-4 md:block"
+            >
+              <BlogReadingIndicator selector=".nuxt-content" />
+            </div>
+          </template>
+
+          <BlogTableOfContents :toc="project.toc" />
+
+          <NuxtContent
+            :document="project"
+            class="max-w-full prose prose-black dark:prose-light"
+          />
+        </div>
+      </article>
+
+      <Disqus
+        v-if="!$config.isDev"
+        :title="project.title"
+        :url="`https://eggsy.xyz/blog/${project.slug}`"
+        :identifier="`/blog/${project.slug}`"
+        :slug="project.slug"
+        lang="tr"
+        class="mt-10"
+      />
+
+      <div class="mt-16 space-y-10">
+        <div v-if="getRelatedPosts.length > 0" class="space-y-2">
+          <h3 class="text-sm dark:text-white/30 text-black/50">
+            Benzer Gönderiler
+          </h3>
+
+          <div v-if="getRelatedPosts.length" class="grid gap-4 sm:grid-cols-2">
+            <NuxtLink
+              v-for="(relatedPost, index) in getRelatedPosts"
+              :key="`related-${index}`"
+              :to="`/blog/${relatedPost.slug}`"
+              class="rounded-lg border-[0.1px] p-4 bg-opacity-25 bg-neutral-300 border-neutral-200 dark:(bg-neutral-800/30 border-neutral-800) flex items-center space-x-2 hover:bg-opacity-40 transition-colors dark:text-white/80 dark:hover:text-white transition-colors"
+            >
+              <IconDocument class="w-4 h-4" />
+              <span class="truncate">{{ relatedPost.title }}</span>
+            </NuxtLink>
+          </div>
+        </div>
+
+        <div class="space-y-2">
+          <BlogPrevNext :current-slug="project.slug" />
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss">
+.dark .prose .nuxt-content-highlight {
+  box-shadow: 0 0 0 100vmax #262626;
+
+  pre {
+    @apply bg-neutral-800;
+  }
+}
+
+.light .nuxt-content-highlight {
+  box-shadow: 0 0 0 100vmax #282c34;
+}
+
+.nuxt-content {
+  .nuxt-content-highlight {
+    clip-path: inset(0 -100vmax);
+
+    @apply mb-5 relative;
+
+    .filename {
+      @apply font-light mt-3 mr-3 text-xs right-0 text-white/50 z-10 absolute;
+    }
+
+    pre {
+      @apply rounded-none py-4 px-0;
+    }
+  }
+
+  ol {
+    @apply pl-0;
+  }
+
+  video {
+    @apply rounded-lg;
+  }
+
+  kbd {
+    @apply rounded-lg bg-black/10 dark:(bg-white/10 border-white/30) px-2 py-1 text-sm border-b-2 border-black/30 cursor-default;
+  }
+
+  code {
+    @apply bg-blue-100 py-px px-1 text-blue-600 dark:bg-white/5;
+
+    &::before,
+    &::after {
+      content: "`";
+    }
+  }
+}
+</style>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,302 +1,117 @@
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue"
+
+/* Interfaces */
+import type {Post} from "@/types/Post"
+import type {FetchReturn} from "@nuxt/content/types/query-builder"
+
+export default Vue.extend({
+  data() {
+    return {
+      formatter: new Intl.DateTimeFormat("tr-TR", {
+        month: "short",
+        day: "numeric",
+      }),
+      query: this.$route.query,
+      projects: [] as (Post[] & FetchReturn) | (Post[] & FetchReturn)[],
+    }
+  },
+  fetchOnServer: false,
+  async fetch() {
+    this.projects = await this.$content("projects")
+      .without(["body"])
+      .fetch<Post[]>()
+  },
   head() {
-    const title = "Projects"
+    const title = "Stephen Chen Projects"
     const description =
-      "Check out my projects and see what I can build for you!"
+      "All my projects in my career"
 
     return {
-      title,
+      title: "Projects",
       meta: this.$prepareMeta({
         title,
         description,
       }),
     }
   },
-  data: () => ({
-    projects: [
-      {
-        client: "Ankara Büyükşehir Belediyesi",
-        name: "Plant App",
-        description:
-          "An app to display plant information that will be placed on a local museum of plants.",
-        image: "https://i.imgur.com/PmmO69d.png",
-      },
-      {
-        client: "Historia Mall",
-        name: "Historia Floor Plan",
-        description:
-          "A floor plan kiosk app for Historia Shopping and Life Center in Fatih, Istanbul.",
-        image: "https://i.imgur.com/gwxvO4o.jpg",
-      },
-      {
-        note: "Acquired!",
-        name: "Discord Server Templates",
-        description:
-          "A website to browse Discord server templates and create your own.",
-        image: "https://i.imgur.com/m64yr7b.jpg",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://discords.com/templates",
-            icon: "IconLink",
-          },
-        ],
-      },
-      {
-        name: "PreMiD",
-        description:
-          "A free, open-source, and community-driven rich presence system.",
-        image: "https://i.imgur.com/1Yc1HB0.png",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://premid.app",
-            icon: "IconLink",
-          },
-          {
-            text: "PreMiD Pages",
-            link: "/projects/premid",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/PreMiD/PreMiD",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Discord Steam Verification",
-        description:
-          "A Discord bot to verify if a user owns a specific item on Steam and give them roles!",
-        image: "https://i.imgur.com/D6SDCdC.png",
-        buttons: [
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/discord-steam-verification",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Unblock Please",
-        description:
-          "Free to use Chrome extension to unblock Imgur and Pastebin restrictions.",
-        image: "https://i.imgur.com/w5wLpFS.jpg",
-        buttons: [
-          {
-            text: "Add to Chrome",
-            link: "https://chrome.google.com/webstore/detail/unblock-please/lpngbkmgnehdkkobdbieckjpphkohffd",
-            icon: "IconInbox",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/unblock-please",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Lanyard Visualizer",
-        description:
-          "A web app to visualize your Discord status with Lanyard API.",
-        image: "https://i.imgur.com/XMfWJlL.jpg",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://visualizer.eggsy.xyz",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/lanyard-visualizer",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Lanyard Web",
-        description:
-          "A website concept for Lanyard API, a real-time Discord status widget.",
-        image: "https://i.imgur.com/V8A5H8f.png",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://lanyard.eggsy.xyz",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/lanyard-web",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        client: "EskiYeni Akademi Tiyatrosu",
-        name: "EskiYeni Tiyatro Website",
-        description: "A website for a local theatre company.",
-        image: "https://i.imgur.com/e09bbgr.jpg",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://eskiyenitiyatro.com",
-            icon: "IconLink",
-          },
-        ],
-      },
-      {
-        name: "Family Feud Game",
-        description:
-          "An interactive browser game with real-time (v2) remote control to play within your community!",
-        image: "https://i.imgur.com/1sQQPPH.png",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://associative-words.netlify.app/",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub (v2)",
-            link: "https://github.com/eggsy/find-associative-words/tree/socket",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Movie App",
-        description:
-          "A movie website/app concept built with TMDb API, Next.js and Tailwind CSS.",
-        image: "https://i.imgur.com/EQfaqte.jpg",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://movie-app-eggsy.netlify.app/",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/movie-app",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        name: "Marmaris Palace Website",
-        description:
-          "A website concept for a 5-star hotel in Marmaris, Türkiye.",
-        image: "https://i.imgur.com/BJ2luOh.jpg",
-        buttons: [
-          {
-            text: "Visit",
-            link: "https://marmaris-palace.netlify.app/",
-            icon: "IconLink",
-          },
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/marmaris-palace-website",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-      {
-        note: "You are here!",
-        name: "eggsy.xyz",
-        description:
-          "My personal website, built with Nuxt.js, Windi CSS and Firebase!",
-        image: "https://i.imgur.com/J4bqPcs.png",
-        buttons: [
-          {
-            text: "GitHub",
-            link: "https://github.com/eggsy/website",
-            icon: "IconBrand:github",
-          },
-        ],
-      },
-    ],
-  }),
-  mounted() {
-    this.$applyMediumZoom()
+  computed: {
+    isFetchPending() {
+      return (
+        this.$fetchState?.pending === true && this.$fetchState.error !== null
+      )
+    },
+    getYearGroupedPosts() {
+      const yearsOfPosts = new Map() as Map<number, Post[]>
+
+      for (const project of this.projects) {
+        if (!project.createdAt) continue
+        const year = new Date(project.createdAt).getFullYear()
+
+        if (yearsOfPosts.has(year)) {
+          yearsOfPosts.get(year)?.push(project as Post)
+        } else {
+          yearsOfPosts.set(year, [project as Post])
+        }
+      }
+
+      const years = [...yearsOfPosts.keys()].sort((a, b) => b - a)
+      const sortedByYears = new Map() as Map<number, Post[]>
+
+      for (const year of years) {
+        sortedByYears.set(year, yearsOfPosts.get(year)!)
+      }
+
+      return sortedByYears
+    },
   },
-}
+  watch: {
+    "$route.query": "setQuery",
+  },
+  mounted() {
+    this.setQuery()
+  },
+  methods: {
+    setQuery() {
+      this.query = this.$route.query
+    },
+  },
+})
 </script>
 
 <template>
-  <PageLayout
-    title="My Projects"
-    description="Check out the projects I worked on!"
-  >
-    <section class="flex items-center flex-wrap gap-2">
-      <span class="text-sm">Jump to:</span>
-      <Button
-        v-for="(project, index) in projects"
-        :key="`skipto-button-${index}`"
-        :href="`#${project.name.toLowerCase().split(' ').join('-')}`"
-      >
-        {{ project.name }}
-      </Button>
-    </section>
+  <LoadersBlog v-if="$fetchState.pending || $fetchState.error !== null" />
 
+  <div v-else class="mt-12 space-y-10">
     <section
-      v-for="(project, index) in projects"
-      :key="`project-${index}`"
-      :id="project.name.toLowerCase().split(' ').join('-')"
-      class="grid md:grid-cols-2 gap-8 md:gap-12 items-center"
-      :class="index === 0 ? 'mt-24 mb-42' : 'my-42'"
+      v-for="[year, projects] in getYearGroupedPosts"
+      :key="year"
+      class="space-y-4"
     >
-      <div class="space-y-4">
-        <span
-          v-if="project.client"
-          class="bg-blue-600/10 block w-max text-sm rounded-lg px-4 text-blue-600 py-1.5 dark:(bg-white/5 text-white/30)"
+      <h1 class="text-3xl font-bold text-black/90 dark:text-white/90">
+        {{ year }}
+      </h1>
+
+      <div class="space-y-3">
+        <NuxtLink
+          v-for="project in projects"
+          :key="project.slug"
+          :to="`/projects/${project.slug}`"
+          class="flex items-start gap-6 card-base rounded-lg"
         >
-          {{ project.client }}
-        </span>
-
-        <span
-          v-if="project.note"
-          class="bg-orange-600/10 block w-max text-sm rounded-lg px-4 text-orange-600 py-1.5 dark:(bg-white/5 text-white/30)"
-        >
-          {{ project.note }}
-        </span>
-
-        <h2 class="text-4xl text-black/90 font-bold dark:text-white/90">
-          {{ project.name }}
-        </h2>
-
-        <p>
-          {{ project.description }}
-        </p>
-
-        <div v-if="project.buttons" class="flex flex-wrap gap-2">
-          <Button
-            v-for="(button, index) in project.buttons"
-            :key="`button-${index}`"
-            :href="button.link"
-            :blank="!button.link.startsWith('/')"
+          <span
+            class="w-[20%] text-black/50 dark:text-white/50 md:w-1/12 flex-shrink-0"
           >
-            <template #icon>
-              <component
-                v-if="button.icon"
-                :is="button.icon.split(':')[0]"
-                v-bind="{
-                  brand: button.icon.split(':')[1],
-                }"
-                class="h-4 w-4"
-              />
-            </template>
+            {{ formatter.format(new Date(project.createdAt)) }}
+          </span>
 
-            {{ button.text }}
-          </Button>
-        </div>
+          <span
+            class="text-blue-600 dark:text-blue-300 font-medium leading-relaxed"
+          >
+            {{ project.title }}
+          </span>
+        </NuxtLink>
       </div>
-
-      <SmartFigure
-        v-if="project.image"
-        :src="project.image"
-        border
-        :class="index % 2 === 0 && 'md:order-first'"
-      />
     </section>
-  </PageLayout>
+  </div>
 </template>
